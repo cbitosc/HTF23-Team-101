080ab1adbb38cd34f31a98dd1c8c2bc64e8be127
Susheel-2004 <susheel.tatikonda@gmail.com>
2023-10-08 02:44:30 +0530

    Login Page update

---
+++ event_manager/src/app/login/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from '@/components/header/Header';
+import Login from "@/components/login";
+
+
+// src/App.js
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
