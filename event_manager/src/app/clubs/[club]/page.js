f0326b2d46426724f7497a20f525611ed7990494
Akuldeep Jakkula <akuldeepj@Akuldeeps-MacBook-Pro.local>
2023-10-08 03:26:21 +0530

    clubs page

--- event_manager/src/app/clubs/[club]/page.js
+++ event_manager/src/app/clubs/[club]/page.js
@@ -2,15 +2,19 @@ import clubs from "@/app/DB/clubs.json";
 import CircularImage from "@/components/CircularImage";
 import Carousel from "@/components/carousel";
 import events from "@/app/DB/events.json";
+import Navbar from "@/components/header/Navbar";
 
 const ClubPage = ({params}) => {
     let image = clubs.find((club) => (club.title == params.club)).image;
 
     return (
+        <div>
+            <Navbar/>
+        
         <div className="mt-10">
             {/* <img src={image} className="h-52 w-52"/> */}
             <CircularImage imageUrl={image} />
-            <h1 className="text-3xl absolute left-[22rem] top-[10rem]">{params.club}</h1>
+            <h1 className="text-3xl absolute left-[22rem] top-[17rem]">{params.club}</h1>
 
             <div className="mt-[3rem]">
                 <h1 className="text-3xl">Upcomming Events</h1>
@@ -18,6 +22,7 @@ const ClubPage = ({params}) => {
                 <Carousel cards={events}/>
             </div>
 
+        </div>
         </div>
     )
 }
