13bbcfb2ad2ad9c5137856686d37e075288a72e3
brohithkr <brohithkr@gmail.com>
2023-10-08 10:52:55 +0530

    text under progress

--- event_manager/src/app/clubs/[club]/page.js
+++ event_manager/src/app/clubs/[club]/page.js
@@ -3,6 +3,7 @@ import CircularImage from "@/components/CircularImage";
 import Carousel from "@/components/carousel";
 import events from "@/app/DB/events.json";
 import Navbar from "@/components/header/Navbar";
+import Editor from "@/components/editor";
 
 const ClubPage = ({params}) => {
     let image = clubs.find((club) => (club.title == params.club)).image;
@@ -16,7 +17,11 @@ const ClubPage = ({params}) => {
             {/* <img src={image} className="h-52 w-52"/> */}
             <CircularImage imageUrl={image} />
             <h1 className="text-3xl absolute left-[22rem] top-[17rem]">{title.charAt(0).toUpperCase() + title.slice(1,title.length)}</h1>
-
+            <Editor 
+            club={params.club}
+            isReadOnly={true}
+            text={"This is a sample text"}
+            />
             <div className="mt-[3rem]">
                 <h1 className="text-3xl">Upcomming Events</h1>
 
