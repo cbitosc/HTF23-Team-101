9904884cff053b01f65b09898bb82b3db2d0706c
brohithkr <brohithkr@gmail.com>
2023-10-08 11:11:27 +0530

    spoc

--- event_manager/src/app/spoc/page.js
+++ event_manager/src/app/spoc/page.js
@@ -1,3 +1,4 @@
+"use client";
 import clubs from "@/app/DB/clubs.json";
 import CircularImage from "@/components/CircularImage";
 import Carousel from "@/components/carousel";
@@ -6,7 +7,7 @@ import Navbar from "@/components/header/Navbar";
 import RoundedImage from "@/components/RoundedImage";
 import Link from 'next/link'; 
 import clubsData from "@/app/DB/clubs_manager.json";
-import ClubInfo from "@/components/clubInfo";
+import ClubInfo from "@/components/ClubInfo";
 
 const spoc = () => {
     return (
@@ -38,10 +39,10 @@ const spoc = () => {
     </div>
     <div className="mt-10 ml-9">
             <Link href="/edit"> {/* Link to the club management page */}
-            <a class="rounded-lg  relative w-36 h-10 cursor-pointer flex items-center border border-green-500 bg-green-500 group hover:bg-green-500 active:bg-green-500 active:border-green-500" href="{{ route('process.create') }}">
-  <span class="text-gray-200 font-semibold ml-8 transform group-hover:translate-x-20 transition-all duration-300 group-hover:hidden">Add Item</span>
-  <span class="absolute right-0 h-full w-10 rounded-lg bg-green-500 flex items-center justify-center transform group-hover:translate-x-0 group-hover:w-full transition-all duration-300">
-    <svg class="svg w-8 text-white" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <a className="rounded-lg  relative w-36 h-10 cursor-pointer flex items-center border border-green-500 bg-green-500 group hover:bg-green-500 active:bg-green-500 active:border-green-500" href="{{ route('process.create') }}">
+  <span className="text-gray-200 font-semibold ml-8 transform group-hover:translate-x-20 transition-all duration-300 group-hover:hidden">Add Item</span>
+  <span className="absolute right-0 h-full w-10 rounded-lg bg-green-500 flex items-center justify-center transform group-hover:translate-x-0 group-hover:w-full transition-all duration-300">
+    <svg className="svg w-8 text-white" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
       <line x1="12" x2="12" y1="5" y2="19"></line>
       <line x1="5" x2="19" y1="12" y2="12"></line>
       </svg>
